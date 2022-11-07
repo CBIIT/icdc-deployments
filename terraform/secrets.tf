762df9066b33635a8d92842b8f446cb23c259a03
Michael Fleming <michael.fleming@nih.gov>
2022-11-07 10:47:11 -0500

    updated variable name

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -3,13 +3,13 @@ variable "neo4j_password" {
   type        = string
   description = "neo4j password"
 }
+variable "neo4j_ip" {
+  type        = string
+  description = "name of the db instance for this tier"
+}
 variable "indexd_url" {
   type        = string
   description = "indexd url"
-}
-variable "db_ip" {
-  type        = string
-  description = "name of the db instance for this tier"
 }
 
 module "secrets" {
@@ -17,7 +17,7 @@ module "secrets" {
   app                           = var.stack_name
   es_host                       = var.create_opensearch_cluster ? module.opensearch[0].opensearch_endpoint : ""
   neo4j_password                = var.neo4j_password
-  db_ip                         = var.db_ip
+  neo4j_ip                      = var.neo4j_ip
   indexd_url                    = var.indexd_url
   sumo_collector_token_frontend = module.monitoring.frontend_source_url
   sumo_collector_token_backend  = module.monitoring.backend_source_url
