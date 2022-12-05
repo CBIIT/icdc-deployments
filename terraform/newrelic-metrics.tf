6b0ef219ff59ac4cc152d260d1ed87e26c9d12ef
Michael Fleming <michael.fleming@nih.gov>
2022-12-05 16:55:57 -0500

    fixed var name

--- terraform/newrelic-metrics.tf
+++ terraform/newrelic-metrics.tf
@@ -35,7 +35,7 @@ module "new_relic_metric_pipeline" {
   app                       = var.stack_name
   http_endpoint_access_key  = var.newrelic_api_key
   level                     = var.account_level
-  new_relic_account_id      = var.new_relic_account_id
+  new_relic_account_id      = var.newrelic_account_id
   permission_boundary_arn   = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/PermissionBoundary_PowerUser"
   program                   = var.program
   s3_bucket_arn             = var.newrelic_s3_bucket
