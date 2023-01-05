17bcd85d7b6d173d6a460fe28e69d4b85efde448
Michael Fleming <michael.fleming@nih.gov>
2023-01-05 14:38:34 -0500

    updated secrets to add

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -5,10 +5,12 @@ locals {
       secretKey = ""
       description = ""
       secretValue = {
-        es_host = var.create_opensearch_cluster ? module.opensearch[0].opensearch_endpoint : ""
-        sumo_collector_token_frontend = module.monitoring.sumo_source_urls.frontend[0]
-        sumo_collector_token_backend  = module.monitoring.sumo_source_urls.backend[0]
-        sumo_collector_token_files    = module.monitoring.sumo_source_urls.files[0]
+        es_host                             = var.create_opensearch_cluster ? module.opensearch[0].opensearch_endpoint : ""
+        sumo_collector_token_frontend       = module.monitoring.sumo_source_urls.frontend[0]
+        sumo_collector_token_backend        = module.monitoring.sumo_source_urls.backend[0]
+        sumo_collector_token_files          = module.monitoring.sumo_source_urls.files[0]
+        sumo_collector_token_interoperation = module.monitoring.sumo_source_urls.interoperation[0]
+      }
       }
     }
   }
