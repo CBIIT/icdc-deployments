f4d504c49086cc1d5ed9e4f81ceed9de045b3803
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-05-18 12:47:24 -0400

    updated

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,7 +1,6 @@
 module "dns" {
   count = var.create_dns_record ? 1: 0
   source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/route53"
-  resource_prefix  = "my-dns"
   env = terraform.workspace
   alb_zone_id = module.alb.alb_zone_id
   alb_dns_name = module.alb.alb_dns_name
