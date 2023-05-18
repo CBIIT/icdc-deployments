ec5fb531054f373f85d225f2a6efe330e6858b0d
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-05-18 12:40:33 -0400

    updated

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -2,6 +2,7 @@
 module "cloudfront" {
   count = var.create_cloudfront ? 1 : 0
   source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/cloudfront"
+  resource_prefix  = "my-cloudfront"
   alarms = var.alarms
   domain_name = var.domain_name
   cloudfront_distribution_bucket_name = var.cloudfront_distribution_bucket_name
