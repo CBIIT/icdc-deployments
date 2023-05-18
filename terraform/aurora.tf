fee117fb7b62beae12f394f273264a352b4becd6
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-05-18 11:57:31 -0400

    updated

---
+++ terraform/aurora.tf
@@ -0,0 +1,15 @@
+module "aurora" {
+  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/aurora"
+  env    =  terraform.workspace
+  stack_name = var.stack_name
+  tags = var.tags
+  vpc_id = var.vpc_id
+  db_engine_mode = var.db_engine_mode
+  db_engine_version = var.db_engine_version
+  db_instance_class = var.db_instance_class
+  db_engine_type = var.db_engine_type
+  master_username = var.master_username
+  allowed_ip_blocks = var.allowed_ip_blocks
+  db_subnet_ids = var.private_subnet_ids 
+  database_name = var.database_name
+}
