9e83f64d5e69503a41744322d886fcdc9f285fc5
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-05-18 11:42:35 -0400

    updated

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -5,26 +5,25 @@ locals {
       secretKey = ""
       description = ""
       secretValue = {
-        es_host                             = var.create_opensearch_cluster ? module.opensearch[0].opensearch_endpoint : ""
-        sumo_collector_token_frontend       = module.monitoring.sumo_source_urls.frontend[0]
-        sumo_collector_token_backend        = module.monitoring.sumo_source_urls.backend[0]
-        sumo_collector_token_files          = module.monitoring.sumo_source_urls.files[0]
-        sumo_collector_token_interoperation = module.monitoring.sumo_source_urls.interoperation[0]
-      }
+        es_host = var.create_opensearch_cluster ? module.opensearch[0].opensearch_endpoint : ""
+        sumo_collector_token_frontend = module.monitoring.sumo_source_urls.frontend[0]
+        sumo_collector_token_backend  = module.monitoring.sumo_source_urls.backend[0]
+        sumo_collector_token_files    = module.monitoring.sumo_source_urls.files[0]
+        sumo_collector_token_interoperation  = module.monitoring.sumo_source_urls.interoperation[0]
+        mysql_host                    = module.aurora.cluster_endpoint
+        mysql_password                = nonsensitive(module.aurora.db_password)
       }
     }
   }
-
 }
-
-#secrets
-variable "secret_values" {
+  variable "secret_values" {
   type = map(object({
-    secretKey = string
-    secretValue = map(string)
-    description = string
+    secretKey               = string
+    secretValue             = map(string)
+    description             = string
   }))
 }
+
 
 module "deepmerge" {
   source  = "Invicton-Labs/deepmerge/null"
@@ -35,7 +34,7 @@ module "deepmerge" {
 }
 
 module "secrets" {
-  source                        = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/secrets?ref=terraform_modules"
+  source                        = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/secrets?ref=v1.0"
   app                           = var.stack_name
   secret_values                 = module.deepmerge.merged
 }
