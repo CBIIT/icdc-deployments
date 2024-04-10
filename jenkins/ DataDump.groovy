e077052c545d28edbf777d4f2aa8918bdec95e1c
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-04-10 17:17:01 -0400

    added data dump and data restore jenkinsfiles, added datadump playbooks

---
+++ jenkins/ DataDump.groovy
@@ -0,0 +1,99 @@
+@Library('datacommons-jenkins-shared-library@v1.1') _
+
+def getLabelForEnvironment(environment) {
+	if (environment == "stage" || environment == "prod"){
+		return "ncias-p2790-c"
+	}else {
+		return "slave-ncias-d2940-c"
+	}
+}
+
+
+pipeline {
+	agent {
+		node {
+			label getLabelForEnvironment(params.Environment)
+		}
+	}
+
+	parameters {
+    extendedChoice( 
+        name: 'Environment', 
+        defaultValue: 'dev', 
+        description: 'Choose the environment to build', 
+        type: 'PT_SINGLE_SELECT',
+        value: 'dev,dev2,qa,stage,prod' )
+    string(defaultValue: "", 
+        description: 'Name of the dump file to use', 
+        name: 'DumpFileName')
+    
+    }
+
+  tools {
+  	maven 'Default' 
+    jdk 'Default' 
+  }
+ environment {
+    DUMP_FILE     = "${params.DumpFileName}"
+	  TIER          = "${params.Environment}"
+    SLACK_SECRET  = "icdc_slack_url"
+    PROJECT       = "icdc"
+    S3_BUCKET     = "crdc-dev-icdc-neo4j-data-backup"
+ }
+  stages{
+
+  	stage('create inventory'){
+ 		steps {
+ 		  wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+			    ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/playbooks/hostfile.yml',
+                inventory: '${WORKSPACE}/ansible/playbooks/hosts',
+                extraVars: [
+                  tier: "${params.Environment}",
+						      project_name: "${PROJECT}",
+                  workspace: "$WORKSPACE"
+						    ],
+                colorized: true)
+		  }
+ 		}
+    
+  }
+  stage("take data dump"){
+    steps{
+      wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+			    ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/playbooks/data-dump.yml',
+                inventory: '${WORKSPACE}/inventory/hosts',
+                // extraVars: [
+                //   tier: "${params.Environment}",
+						    //   project_name: "${PROJECT}",
+                //   workspace: "$WORKSPACE"
+						    // ],
+                colorized: true)
+		  }
+    }
+  }
+	stage('push to s3'){
+		steps{			
+			ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/playbooks/data-dump-push.yml',
+                inventory: '${WORKSPACE}/ansible/playbooks/hosts',
+				        credentialsId: 'commonsdocker',
+                colorized: true)
+
+		}
+	}
+	
+  }
+  post {
+    always {
+       notify(
+            secretPath: "notification/slack",
+            secretName: "${env.SLACK_SECRET}"
+        ) 
+      }
+    cleanup {
+      cleanWs()
+      }
+  }
+}
---
+++ jenkins/DataRestore.groovy
@@ -0,0 +1,94 @@
+
+@Library('datacommons-jenkins-shared-library@v1.1') _
+
+def getLabelForEnvironment(environment) {
+	if (environment == "stage" || environment == "prod"){
+		return "ncias-p2790-c"
+	}else {
+		return "slave-ncias-d2940-c"
+	}
+}
+
+
+pipeline {
+	agent {
+		node {
+			label getLabelForEnvironment(params.Environment)
+		}
+	}
+
+	parameters {
+    extendedChoice( 
+        name: 'Environment', 
+        defaultValue: 'dev', 
+        description: 'Choose the environment to build', 
+        type: 'PT_SINGLE_SELECT',
+        value: 'dev,dev2,qa,stage,prod' )
+    string(defaultValue: "", 
+        description: 'Name of the dump file to use', 
+        name: 'DumpFileName')
+    
+    }
+
+  tools {
+  	maven 'Default' 
+    jdk 'Default' 
+  }
+ environment {
+    DUMP_FILE = "${params.DumpFileName}"
+	  TIER      = "${params.Environment}"
+    SLACK_SECRET  = "icdc_slack_url"
+    PROJECT       = "icdc"
+    S3_BUCKET     = "crdc-dev-icdc-neo4j-data-backup"
+ }
+  stages{
+
+  	stage('create inventory'){
+ 		steps {
+ 		  wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+			    ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/playbooks/hostfile.yml',
+                inventory: '${WORKSPACE}/ansible/playbooks/hosts',
+                extraVars: [
+                  tier: "${params.Environment}",
+						      project_name: "${PROJECT}",
+                  workspace: "$WORKSPACE"
+						    ],
+                colorized: true)
+		  }
+ 		}
+    
+  }
+  stage("download dump"){
+    steps{
+      wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+			    ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/playbooks/download-dump.yml',
+                inventory: '${WORKSPACE}/ansible/playbooks/hosts',
+                colorized: true)
+		  }
+    }
+  }
+	stage('restore data dump'){
+		steps{			
+			ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/playbooks/dump-restore.yml',
+                inventory: '${WORKSPACE}/inventory/hosts',
+                colorized: true)
+
+		}
+	}
+	
+  }
+  post {
+    always {
+       notify(
+            secretPath: "notification/slack",
+            secretName: "${env.SLACK_SECRET}"
+        ) 
+      }
+    cleanup {
+      cleanWs()
+      }
+  }
+}
