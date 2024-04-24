7a02bc8df1492670aca2c47317f11f641e95f290
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-04-24 13:05:20 -0400

    updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,22 +11,3 @@ provider "aws" {
   region = var.region
 }  
   
-  /*default_tags {
-    tags = {
-      EnvironmentTier = terraform.workspace
-      Customer        = "nci od cbiit ods"
-      DevLead         = "Karan Sheth"
-      CreatedBy       = "Charles Ngu"
-      ResourceName    = "NCI-icdc-${terraform.workspace}"
-      FISMA           = "moderate"
-      ManagedBy       = "terraform"
-      OpsModel        = "cbiit managed hybrid"
-      Program         = "crdc"
-      PII             = "yes"
-      Backup          = local.level
-      PatchGroup      = local.level
-      ApplicationName = "Integrated Canine Data Commons"
-      ProjectManager  = "Gina Kuffel"
-    }
-  }
-}*/
