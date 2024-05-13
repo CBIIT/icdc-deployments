b34e42448cd774a1ea6c1a640a234e68c3cdf23f
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-05-13 14:35:25 -0400

    updated

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,10 +17,16 @@ output "eventbridge_rule_name" {
   description = "Name of the EventBridge rule created for the ECS task scheduler."
 }
 
-output "eventbridge_target_id" {
-  value = module.event_scheduler.eventbridge_target_id
-  description = "Target ID of the EventBridge target created for the ECS task scheduler."
+output "eventbridge_ecs_target_id" {
+  value = module.event_scheduler.eventbridge_ecs_target_id
+  description = "The ID of the EventBridge ECS target."
 }
+
+output "eventbridge_lambda_target_id" {
+  value = module.event_scheduler.eventbridge_lambda_target_id
+  description = "The ID of the EventBridge Lambda target."
+}
+
 
 #output "read_only_role_arn" {
  # value = module.new_relic_metric_pipeline.*.read_only_role_arn
