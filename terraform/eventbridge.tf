6880035170898ee573506a7a713a85ea5563ea9c
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-05-13 12:38:34 -0400

    updated

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,12 +1,13 @@
 module "eventbridge_ecs_schedule" {
   source              = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/eventbridge?ref=eventbridge_new"
   resource_prefix     = "${var.stack_name}-${terraform.workspace}"
-  name    = var.eventbridge_name
+  eventbridge_name   = var.eventbridge_name
   schedule_expression = var.schedule_expression // Scheduled expression for every day at 9 AM,
   target_type         = var.target_type
   target_arn          = aws_events_rule.my_ecs_task_rule.arn
   ecs_cluster_arn     = var.ecs_cluster_arn 
   task_definition_arn = var.task_definition_arn
+  vpc_id           = var.vpc_id
   subnets          = var.private_subnet_ids
   ecs_security_groups     = var.ecs_security_groups
   assign_public_ip    = var.assign_public_ip
