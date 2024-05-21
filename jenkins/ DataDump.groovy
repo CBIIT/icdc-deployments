c6b169fb8b952ab859ec5c3025287b857d994cdb
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-05-21 16:24:05 -0400

    updated

--- jenkins/ DataDump.groovy
+++ jenkins/ DataDump.groovy
@@ -38,7 +38,7 @@ pipeline {
 	  TIER          = "${params.Environment}"
     SLACK_SECRET  = "icdc_slack_url"
     PROJECT       = "icdc"
-    S3_BUCKET     = "crdc-{{ project }}-{{ tier }}-neo4j-data-backup"
+    S3_BUCKET     = "crdc-${env.PROJECT}-${env.TIER}-neo4j-data-backup"
  }
   stages{
 
