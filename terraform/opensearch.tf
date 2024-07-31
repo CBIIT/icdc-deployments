e95e90a9594d710f758c95e562e1b789ee908464
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-07-31 12:01:47 -0400

    updated

--- terraform/opensearch.tf
+++ terraform/opensearch.tf
@@ -1,7 +1,8 @@
 #create opensearch
 module "opensearch" {
   count = var.create_opensearch_cluster ? 1: 0
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/opensearch"
+  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/opensearch?ref=v1.9"
+  resource_prefix     = "${var.stack_name}-${terraform.workspace}"
   stack_name = var.stack_name
   tags = var.tags
   opensearch_instance_type = var.opensearch_instance_type
