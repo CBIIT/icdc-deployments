71a25d130aea5d2b56c15a0ecfbc23f9293a5919
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2025-04-28 19:37:28 -0400

    updated

--- awscdk/bento/app/aspects.py
+++ awscdk/bento/app/aspects.py
@@ -7,7 +7,6 @@ from aws_cdk import aws_iam as iam
 @jsii.implements(cdk.IAspect)
 class MyAspect:
     def visit(self, node):
-
         # Read config file
         config = ConfigParser()
         config.read('config.ini')
@@ -15,6 +14,12 @@ class MyAspect:
         if isinstance(node, iam.CfnRole):
             if config.has_option('iam', 'role_prefix'):
                 resolvedLogicalId = cdk.Stack.of(node).resolve(node.logical_id)
-                #roleName = config['iam']['role_prefix'] + '-' + config['main']['resource_prefix'] + '-' + resolvedLogicalId
-                roleName = config['iam']['role_prefix'] + '-' + resolvedLogicalId[:30] 
-                node.role_name = roleName
+                base_role_name = config['iam']['role_prefix'] + '-' + resolvedLogicalId
+
+                # IAM role name must be <= 64 characters
+                if len(base_role_name) > 64:
+                    max_logical_id_length = 64 - len(config['iam']['role_prefix']) - 1 
+                    resolvedLogicalId_truncated = resolvedLogicalId[:max_logical_id_length]
+                    base_role_name = config['iam']['role_prefix'] + '-' + resolvedLogicalId_truncated
+
+                node.role_name = base_role_name
